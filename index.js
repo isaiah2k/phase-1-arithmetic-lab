7a2b19ca16c6d8a9e23c82e8032746793a957615
Isaiah <zaymgmt707@gmail.com>
2024-06-17 22:42:20 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+const min = 1;
+const maxValue = 10
+const random = Math.floor(Math.random() * (maxValue - min + 1)) + min
+const num3 = 12
+const num4 = 3
+const mod = num3 / num4
+const max = Math.max(18,19,20)
