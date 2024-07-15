6cbcb53ec73cb14f354ee94bbafeecf3a8a9c2c3
Isaiah <zaymgmt707@gmail.com>
2024-07-14 21:30:01 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// Defines 2 numbers
 const num1 = 31
 const num2 = 2
+// Multiplies num1 and num2 and stores the result in the variable `multiply`
 const multiply = num1 * num2
-const min = 1;
-const maxValue = 10
-const random = Math.floor(Math.random() * (maxValue - min + 1)) + min
-const num3 = 12
-const num4 = 3
-const mod = num3 / num4
+// Generates a random integer between 0 and 1 multiplies it by 10 and adds 1 then rounds down to nearest whole number
+const random = Math.floor(Math.random() * 10+ 1)
+// Defines 2 numbers for remainder function
+const num3 = 24
+const num4 = 5
+// Function to solve a remainder equal to 4
+const mod = num3 % num4
+// Finds highest number in array
 const max = Math.max(18,19,20)
